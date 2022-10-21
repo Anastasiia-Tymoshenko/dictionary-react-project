4c207c22af7e3f4cb290a70657a248c1eb159022
Anastasiia Tymoshenko <nastyatmnk@gmail.com>
2022-10-22 00:03:41 +0100

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Dictionary from './Dictionary.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer></footer>
+      </div>
     </div>
   );
 }
