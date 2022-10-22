49b91d3b1349816e08e74558a3efa915be886087
Anastasiia Tymoshenko <nastyatmnk@gmail.com>
2022-10-23 00:59:35 +0100

    Added the word meaning displaying

---
+++ src/Results.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Meaning from './Meaning';
+
+export default function Results(props) {
+  console.log(props.result);
+  if (props.results) {
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            //meaning.definitions[0].definition;
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
